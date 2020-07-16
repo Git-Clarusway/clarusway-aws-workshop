0828d1aa96ce0584595c77e29e31420501f575a1
Yaz <kaplan3260@gmail.com>
2020-07-15 23:46:44 -0500

    changed content

--- 002-milliseconds-converter/app.py
+++ 002-milliseconds-converter/app.py
@@ -1,11 +1,29 @@
 from flask import *
 app=Flask(__name__)
+def milli_sn(x):
+    a=int(x)
+    x=''
+    if a>999:
+        while a>999:    
+            if a>=3600000:
+                b,a=divmod(a,3600000)
+                x+=f'{b} hour/s'
+            elif a>=60000:
+                b,a=divmod(a,60000)
+                x+=f' {b} minute/s'
+            else:
+                b,a=divmod(a,1000)
+                x+=f' {b} second/s'
+    else:
+            x+=f'just {a} millisecond/s'
+    return x         
 @app.route('/')
 def home():
     return redirect('home')
 @app.route('/home')
 def index():
-    return render_template('index.html',developer_name='Developer_Name',not_valid=False)
+    return render_template('index.html',developer_name='SK',not_valid=False)
+    
 @app.route('/',methods=['POST','GET'])    
 def result():
     if request.method=='POST':
@@ -16,22 +34,9 @@ def result():
                 return render_template('index.html',developer_name='Developer_Name',not_valid=True)
         else:
             return render_template('index.html',developer_name='Developer_Name',not_valid=True)
-        x=''
-        if a>999:
-            while a>999:    
-                    if a>=3600000:
-                        b,a=divmod(a,3600000)
-                        x+=f'{b} hour/s'
-                    elif a>=60000:
-                        b,a=divmod(a,60000)
-                        x+=f' {b} minute/s'
-                    else:
-                        b,a=divmod(a,1000)
-                        x+=f' {b} second/s'
-        else:
-                x+=f'just {a} millisecond/s'    
-        return render_template('result.html',milliseconds=request.form['number'],result=x,developer_name='Developer_Name',not_valid=False)
+          
+        return render_template('result.html',milliseconds=request.form['number'],result=milli_sn(a),developer_name='SK',not_valid=False)
 
 if __name__=='__main__':
-    # app.run(debug=True)
-    app.run('0.0.0.0',port=80)
+    app.run(debug=True) 
+    # app.run('0.0.0.0',port=80)
